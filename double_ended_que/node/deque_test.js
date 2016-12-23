0910f0b06560b96b11a65db69a60654243e91200
Rajasimhan <rajasimhan.shanmugam@gmail.com>
2016-12-23 11:57:53 +0530

    changes

--- double_ended_que/node/deque_test.js
+++ double_ended_que/node/deque_test.js
@@ -1,8 +1,8 @@
 var deque_module = require('./deque.js');
 var dq = new deque_module();
-dq.readFront();
+console.log("Front is " + dq.readFront());
 dq.pushBack(23);
 dq.pushBack(34);
 dq.pushBack(38);
-dq.readFront();
-dq.readBack();
+console.log("Front is " + dq.readFront());
+console.log("Back is " + dq.readBack());
